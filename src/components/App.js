34116fa07d39a24bbb2002da56a284cddbc72bac
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-12-24 01:24:20 -0700

    Creates component to list entire videos returned from API call

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,54 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
   state = {
     videos: [],
+    selectedVideo: null,
   };
+
+  //Serach for a default term when the application first loads
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onUserSubmit={this.onTermSubmit} />
+
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
